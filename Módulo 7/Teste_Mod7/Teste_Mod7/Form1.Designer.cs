15681235ac7420076b8f6eeee5e527ef6560694e
Leonardo Gabriel Vicente Pinheiro <leonardogabrielpinheiro@hotmail.com>
2017-10-17 15:41:15 +0100

    Added Mod_7_Test (Caeser Cipher)

---
+++ Módulo 7/Teste_Mod7/Teste_Mod7/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Teste_Mod7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.abrirFicheiroBtn = new System.Windows.Forms.Button();
+            this.guardarFicheiroBtn = new System.Windows.Forms.Button();
+            this.EncriptarBtn = new System.Windows.Forms.Button();
+            this.DesencriptarBtn = new System.Windows.Forms.Button();
+            this.NovoBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // abrirFicheiroBtn
+            // 
+            this.abrirFicheiroBtn.Location = new System.Drawing.Point(12, 12);
+            this.abrirFicheiroBtn.Name = "abrirFicheiroBtn";
+            this.abrirFicheiroBtn.Size = new System.Drawing.Size(91, 42);
+            this.abrirFicheiroBtn.TabIndex = 0;
+            this.abrirFicheiroBtn.Text = "Abrir Ficheiro";
+            this.abrirFicheiroBtn.UseVisualStyleBackColor = true;
+            this.abrirFicheiroBtn.Click += new System.EventHandler(this.abrirFicheiroBtn_Click);
+            // 
+            // guardarFicheiroBtn
+            // 
+            this.guardarFicheiroBtn.Location = new System.Drawing.Point(12, 60);
+            this.guardarFicheiroBtn.Name = "guardarFicheiroBtn";
+            this.guardarFicheiroBtn.Size = new System.Drawing.Size(91, 42);
+            this.guardarFicheiroBtn.TabIndex = 1;
+            this.guardarFicheiroBtn.Text = "Guardar Ficheiro";
+            this.guardarFicheiroBtn.UseVisualStyleBackColor = true;
+            this.guardarFicheiroBtn.Click += new System.EventHandler(this.guardarFicheiroBtn_Click);
+            // 
+            // EncriptarBtn
+            // 
+            this.EncriptarBtn.Location = new System.Drawing.Point(12, 108);
+            this.EncriptarBtn.Name = "EncriptarBtn";
+            this.EncriptarBtn.Size = new System.Drawing.Size(91, 42);
+            this.EncriptarBtn.TabIndex = 2;
+            this.EncriptarBtn.Text = "Encriptar";
+            this.EncriptarBtn.UseVisualStyleBackColor = true;
+            this.EncriptarBtn.Click += new System.EventHandler(this.EncriptarBtn_Click);
+            // 
+            // DesencriptarBtn
+            // 
+            this.DesencriptarBtn.Location = new System.Drawing.Point(12, 156);
+            this.DesencriptarBtn.Name = "DesencriptarBtn";
+            this.DesencriptarBtn.Size = new System.Drawing.Size(91, 42);
+            this.DesencriptarBtn.TabIndex = 3;
+            this.DesencriptarBtn.Text = "Desencriptar";
+            this.DesencriptarBtn.UseVisualStyleBackColor = true;
+            this.DesencriptarBtn.Click += new System.EventHandler(this.DesencriptarBtn_Click);
+            // 
+            // NovoBtn
+            // 
+            this.NovoBtn.Location = new System.Drawing.Point(12, 204);
+            this.NovoBtn.Name = "NovoBtn";
+            this.NovoBtn.Size = new System.Drawing.Size(91, 42);
+            this.NovoBtn.TabIndex = 4;
+            this.NovoBtn.Text = "Novo";
+            this.NovoBtn.UseVisualStyleBackColor = true;
+            this.NovoBtn.Click += new System.EventHandler(this.NovoBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(109, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(563, 234);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 258);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.NovoBtn);
+            this.Controls.Add(this.DesencriptarBtn);
+            this.Controls.Add(this.EncriptarBtn);
+            this.Controls.Add(this.guardarFicheiroBtn);
+            this.Controls.Add(this.abrirFicheiroBtn);
+            this.Name = "Form1";
+            this.Text = "Encriptador 2000";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button abrirFicheiroBtn;
+        private System.Windows.Forms.Button guardarFicheiroBtn;
+        private System.Windows.Forms.Button EncriptarBtn;
+        private System.Windows.Forms.Button DesencriptarBtn;
+        private System.Windows.Forms.Button NovoBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Módulo 7/Teste_Mod7/Teste_Mod7/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Teste_Mod7
+{
+    public partial class Form1 : Form
+    {
+        public string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NovoBtn_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void abrirFicheiroBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Ficheiros de Texto(*.txt)|*.txt";
+            openFileDialog1.ShowDialog();
+            path = openFileDialog1.FileName;
+            try
+            {
+                if (File.Exists(path))
+                {
+                    richTextBox1.Text = File.ReadAllText(path);
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.ToString());
+
+            }
+        }
+
+        private void guardarFicheiroBtn_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            path = saveFileDialog1.FileName;
+            if (path != String.Empty)
+            {
+                File.WriteAllText(path, richTextBox1.Text);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private static char Cripta(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - offset) % 26) + offset);
+        }
+
+        public static string Encriptar(string input, int key)
+        {
+            string output = string.Empty;
+            
+            foreach (char ch in input)
+                output += Cripta(ch, key);
+
+            return output;
+        }
+
+        public static string Desencriptar(string input, int key)
+        {
+            return Encriptar(input, 26 - key);
+        }
+
+        private void EncriptarBtn_Click(object sender, EventArgs e)
+        {
+           richTextBox1.Text= Encriptar(richTextBox1.Text,3);
+        }
+
+        private void DesencriptarBtn_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = Desencriptar(richTextBox1.Text, 3);
+        }
+    }
+}
